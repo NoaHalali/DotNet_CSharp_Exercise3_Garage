8251d610cd2ac807459bbcf822281a5f149114ad
ERAGON9 <liorbarak99@gmail.com>
2024-06-10 12:26:21 +0300

    Create empty classes and start fill them.

---
+++ Ex03.GarageLogic/FuelVehicle.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex03.GarageLogic
+{
+    internal abstract class FuelVehicle : Vehicle
+    {
+        private enum eFuelType
+        {
+            Octan95,
+            Octan96,
+            Octan98,
+            Soler,
+        }
+
+        private float m_FuelAmount;
+        private float m_MaxFuelAmount;
+
+
+    }
+}
---
+++ Ex03.GarageLogic/Vehicle.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex03.GarageLogic
+{
+    internal abstract class Vehicle
+    {
+        private string m_Model;
+        private string m_LicensePlate;
+        private float m_EnergyPrecentage;
+        private List<Wheel> m_Wheels;
+
+
+
+    }
+}
